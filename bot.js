ed6be0afe4c5e8aeb17b5e61c52324b711ccecbf
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-03-02 16:16:31 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,55 @@
+global.Discord = require('discord.js')
+const client = new Discord.Client();
+const fs = require('fs')
+client.login('Ваш токен')
+client.owner = 'Ваш Айди';
+client.on('ready', () => {
+    client.user.setActivity('Ваш Статус', 'Ваш статус')
+})
+client.on('guildMemberAdd', member => {
+    var channel = member.guild.channels.get('Айди канала')
+   message.channel.send(`**${member}, Добро пожаловать на сервер ${member.guild.name}**`)
+})
+    client.on('guildMemberRemove', member => {
+        var channel = member.guild.channel1s.get('Айди канала')
+        message.channel.send(`**Прощай, ${member.user.tag}, желаем тебе удачи!**`)
+})
+client.on('messageDelete', message => {
+    var channel = message.channel.send
+    var embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription(`**Удалено сообщения**\n**Содержание:** ${message.content}\n**ID:** ${message.id}`)
+    .setColor('#FF0000')
+    .setFooter('Ваше Сообщение', client.user.avatarURL)
+   message.channel.send(embed)
+})
+client.prefix = 'Ваш Префикс'
+client.commands = new Discord.Collection();
+fs.readdir('./cmds/', (err, files) => {
+    if(err) console.log(err)
+    let jsfiles = files.filter(f => f.split('.').pop() === 'js');
+    if(jsfiles.length <= 0) {
+        console.log('No commands!')
+        return
+    }
+    console.log(`[COMMANDS] Loaded ${files.length} commannds!`)
+    jsfiles.forEach(f => {
+        let props = require(`./cmds/${f}`)
+        client.commands.set(props.help.name, props);
+    })
+})
+client.on('message', message => {
+    let msg = message.content.toLowerCase() || message.content.toUpperCase()
+    if(!msg.startsWith(client.prefix)) return;
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+    const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
+
+    let command = args.shift().toLowerCase();
+    let cmd;
+    if(client.commands.has(command)) {
+        cmd = client.commands.get(command)
+    }
+    if(!cmd) return;
+    cmd.run(client, message, args)
+})
