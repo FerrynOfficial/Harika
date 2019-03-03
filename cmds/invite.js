bbe10aa445f9e51fa39a5727acf52622dd6c7ab8
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-03-03 09:51:19 +0100

    Create invite.js

---
+++ cmds/invite.js
@@ -0,0 +1,14 @@
+exports.run = (client, message, args) => {
+    const moment = require('moment')
+    const Discord = require('discord.js')
+    var embed = new Discord.RichEmbed()
+    .setTitle(`Invite link`)
+    .setColor("#00FFFF")
+    .setFooter(`Harika | Invite`, client.user.avatarURL)
+    .setDescription(`**Ссылка на приглашение:** https://discordapp.com/oauth2/authorize?client_id=548501008167206922&scope=bot&permissions=8`)
+    .setThumbnail(client.user.avatarURL)
+  message.channel.send(embed)
+    }
+exports.help = {
+    name: 'invite'
+   }   
