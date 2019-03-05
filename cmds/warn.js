369cf42ff9074078654c72d2992b8a36cea4fb59
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-03-05 13:22:49 +0100

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -3,16 +3,16 @@ exports.run = (client, message, args) => {
     var kickmember = message.guild.member(message.mentions.users.first())
     var reason = args.slice(1).join(' ')
 
-if(!message.guild.me.permissions.has('MANAGE_SERVER')) {
+if(!message.guild.me.permissions.has('MANAGE_GUILD')) {
     return message.reply('**У меня нет прав на это.**')
 }
-if(!message.member.permissions.has('MANAGE_SERVER')) {
+if(!message.member.permissions.has('MANAGE_GUILD')) {
     return message.reply('**У вас нет прав на это.**')
 }
 if(!kickmember) {
     return message.reply('**Пользователь не найден**\n`$warn @User Причина`')
 }
-if(kickmember.permissions.has('MANAGE_SERVER')) {
+if(kickmember.permissions.has('MANAGE_GUILD')) {
     return message.reply('**Данный пользователь имеет права равные вам.**')
 }
 if(!reason) {
