f87ed3a7c652db9fd891aac518de7f03f9115caa
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-03-14 17:40:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,58 @@ client.on('guildMemberAdd', member => {
     client.on('guildMemberRemove', member => {
         var channel = member.guild.channel1s.get('548924952766578698')
         channel.send(`**Прощай, ${member.user.tag}, желаем тебе удачи!**`)
+const Discord = require('discord.js');
+const client = new Discord.Client();
+function hook(channel, title, message, color, avatar) { // This function uses quite a few options. The last 2 are optional.
+
+    if (!channel) return message.reply('Укажите канал.');
+    if (!title) return message.reply('Укажите заглавие.');
+    if (!message) return message.reply('Укажите сообщения.');
+    if (!color) color = 'd9a744'; 
+    if (!avatar) avatar = 'https://cdn4.iconfinder.com/data/icons/technology-devices-1/500/speech-bubble-128.png' 
+
+    color = color.replace(/\s/g, '');
+    avatar = avatar.replace(/\s/g, '');
+
+    channel.fetchWebhooks() 
+        .then(webhook => {
+
+            let foundHook = webhook.find('name', 'Webhook'); 
+
+            if (!foundHook) {
+                channel.createWebhook('Webhook', 'https://cdn4.iconfinder.com/data/icons/technology-devices-1/500/speech-bubble-128.png') // Make sure this is the same thing for when you search for the webhook. The png image will be the default image seen under the channel. Change it to whatever you want.
+                    .then(webhook => {
+                        webhook.send('', {
+                            "username": title,
+                            "avatarURL": avatar,
+                            "embeds": [{
+                                "color": parseInt(`0x${color}`),
+                                "description":message
+                            }]
+                        })
+                            .catch(error => { 
+                                console.log(error);
+                                return channel.send('**Something went wrong when sending the webhook. Please check console.**');
+                            })
+                    })
+            } else { 
+                foundHook.send('', { 
+                    "username": title,
+                    "avatarURL": avatar,
+                    "embeds": [{
+                        "color": parseInt(`0x${color}`),
+                        "description":message
+                    }]
+                })
+                    .catch(error => { 
+                        console.log(error);
+                        return channel.send('**Something went wrong when sending the webhook. Please check console.**');
+                    })
+                }
+
+        })
+
+}
 })
 client.on('messageDelete', message => {
     var channel = message.channel.send
