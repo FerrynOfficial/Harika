a70d4a3fd7ae8903ed011551fe40fa5479dd4b8a
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-03-17 12:06:15 +0100

    Update bot-news.js

--- cmds/bot-news.js
+++ cmds/bot-news.js
@@ -3,10 +3,13 @@ exports.run = (client, message, args) => {
     const moment = require('moment')
     var embed = new Discord.RichEmbed()
     .setTitle('Что нового?')
-    .setDescription('Была добавлена команда `news`. Она показывает последние новости на сервере `FlameCommunity`')
+    .setDescription('Была добавлена команда `help`.')
     .setColor('RANDOM')
     message.channel.send(embed)
 }
 exports.help = {
-    name:'bot-news'
+group: 'general',
+desc: 'Показывает новости бота',
+usage: 'bot-news',
+name:'bot-news'
 }
