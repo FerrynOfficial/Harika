99d9815be6e705ce1431a94e20ec730b09073f0c
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-03-17 12:20:53 +0100

    Create help.js

---
+++ cmds/help.js
@@ -0,0 +1,28 @@
+onst Discord = require('discord.js')
+exports.run = (client, message, args) => {
+    var args1 = message.content.split(' ')
+    if(!client.commands.has(args1[1])) {
+        var embed = new Discord.RichEmbed()
+        .setColor(client.color)
+        .addField('Основное', client.commands.filter(cmd => cmd.help.group === 'general').map(cmd => `$${cmd.help.name} - ${cmd.help.desc}`).join('\n'))
+        .addField('Модерация', client.commands.filter(cmd => cmd.help.group === 'moderation').map(cmd => `$${cmd.help.name} - ${cmd.help.desc}`).join('\n'))
+        message.channel.send(embed)
+    } else {
+        var name = client.commands.get(args1[1]).help.name
+        var desc = client.commands.get(args1[1]).help.desc
+        var usage = client.commands.get(args1[1]).help.usage
+        var commandinfo = new Discord.RichEmbed()
+        .setColor(client.color)
+        .addField('Название', name, true)
+        .addField('Описание', desc, true)
+        .addField('Использование', usage, true)
+        message.channel.send(commandinfo)
+
+    }
+}
+exports.help = {
+name: 'help',
+group: 'general',
+desc: 'Показывает все комманды бота',
+usage: 'help <комманда>',
+}
