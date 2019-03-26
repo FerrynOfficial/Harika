83ac890ea650e6cd1e4001bd9a4cd24ae5b93ee1
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-03-26 15:45:16 +0100

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,10 +3,14 @@ exports.run = (client, message, args) => {
     var args1 = message.content.split(' ')
     if(!client.commands.has(args1[1])) {
         var embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setDescription('Меню помощи было выслано вам в ЛС.')
+        message.channel.send(embed)
         .setColor(client.color)
+        .setTitle('Помощь | Help')
         .addField('Основное', client.commands.filter(cmd => cmd.help.group === 'general').map(cmd => `$${cmd.help.name} - ${cmd.help.desc}`).join('\n'))
         .addField('Модерация', client.commands.filter(cmd => cmd.help.group === 'moderation').map(cmd => `$${cmd.help.name} - ${cmd.help.desc}`).join('\n'))
-        message.channel.send(embed)
+        message.member.send(embed)
     } else {
         var name = client.commands.get(args1[1]).help.name
         var desc = client.commands.get(args1[1]).help.desc
