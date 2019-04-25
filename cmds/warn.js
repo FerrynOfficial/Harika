e4841bd3c2a81ac11f9024375bdf520cf031a0f0
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-04-25 11:25:11 +0200

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -1,34 +1,70 @@
 exports.run = (client, message, args) => {
+  const Discord = require('discord.js')
+  const fs = require('fs')
+  var profile = require('./profile.json')
+  var warnmember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0])) 
+
     if(!message.guild.me.permissions.has('KICK_MEMBERS')) {
-        message.reply('**У меня нет прав на это.**')
+      var err = new Discord.RichEmbed()
+      .setTitle('Ошибка')
+      .setDescription('У меня недостаточно прав для выполнения данного действия')
+      .setColor('RANDOM')
+      message.channel.send(err)
         return
     }
     if(!message.member.permissions.has('KICK_MEMBERS')) {
-        message.reply('**У вас нет прав на это.**')
+      var err1 = new Discord.RichEmbed()
+      .setTitle('Ошибка')
+      .setDescription('У вас недостаточно прав для выполнения данного действия')
+      .setColor('RANDOM')
+      message.channel.send(err1)
         return
     }
-    var warnmember = message.guild.member(message.mentions.users.first())
     if(!warnmember) {
-        message.reply('**Пользователь не найден**')
+      var err2 = new Discord.RichEmbed()
+      .setTitle('Ошибка')
+      .setDescription('Вы не указали пользователя')
+      .setColor('RANDOM')
+      message.channel.send(err2)
         return
     }
     if(warnmember.permissions.has('KICK_MEMBERS')) {
-        message.reply('**Данный пользователь имеет права равные вам.**')
+      var err3 = new Discord.RichEmbed()
+      .setTitle('Ошибка')
+      .setDescription('Данный пользователь имеет права равные вам')
+      .setColor('RANDOM')
+      message.channel.send(err3)
         return
     }
     var reason = args.slice(1).join(' ')
     if(!reason) {
-        message.reply('**Укажите причину.**')
+      var err4 = new Discord.RichEmbed()
+      .setTitle('Ошибка')
+      .setDescription('Вы не указали причину')
+      .setColor('RANDOM')
+      message.channel.send(err4)
         return
+        
+       profile[warnmember.id].warns++
+       fs.writeFile('./profile.json')JSON.stringify(profile), (err)=>{
+         if(err) console.log(err)
+       }
     }
     var embed = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
     .setTitle('Предупреждение')
-    .setDescription(`**${warnmember} был предупрежден!\n Причина: ${reason}**`)
+    .addField('Администратор', meessage.author.username)
+    .addField('Нарушитель', `${warnmember.user.username}`)
+    .addField('Количество Предупреждений', `${profile[warnmember.id].warns}/3`)
+    .addField('Причина', `${reason}`)
+    .setColor('RANDOM')
     message.channel.send(embed)
+
+    profile[warnmember.id].warns++
+    if(profile[warnmember.id].warns >=3) {
+      meessage.guild.member(warnmember).kick('3/3 Предупреждений')
+    }
 }
 exports.help = {
-    group: 'general',
     group: 'moderation',
     desc: 'Выдает Предупреждение',
     usage: '$warn [Пользователь] [Причина]',
